8d2a8041b4c6254405e08ceafd6f1f1c5b7fc688
Pihlqvist <fpih@kth.se>
2019-11-11 16:39:42 +0100

    Create  Hook

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,6 +1,16 @@
 import FirebaseContext from './context';
 import Firebase from './firebase';
+import React, {useContext} from 'react';
 
 export default Firebase;
 
-export { FirebaseContext };
+/**
+ * React Hook that uses `FirebaseContext` to access
+ * the `Firebase` class.
+ * @return {Firebase} Firebase Context
+ */
+const useFirebase = () => {
+  return useContext(FirebaseContext);
+}
+
+export { FirebaseContext, useFirebase };
