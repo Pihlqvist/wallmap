43ec1e5f63937becce362aacb5dd02920d01c047
Ali El Tom <23627669+eltomali@users.noreply.github.com>
2019-12-03 15:13:16 +0100

    add description of WallMap in landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -12,6 +12,16 @@ const Landing = () => {
         <div className="BtnSignUp LinkBtn1">
           <Link to={ROUTES.SIGN_UP}>Get Started Now</Link>
         </div>
+        <div className="LandingTextArea">
+          <div className="TextBox">
+            <h3>No more need for a paper wall map</h3>
+            <p>With WallMap you don't need any longer to buy an expensive paper map to hang on the wall and then buy pins to pin every place you have travelled to. You can do it all digitally in WallMap, and very easily. </p>
+          </div>
+          <div className="TextBox">
+            <h3>Save your memories on WallMap</h3>
+            <p>With WallMap we help you keep your memories of each place you have travelled to in the world. You can add photos and info to each place you visited. You simply add a pin to your WallMap and then add your images and info.</p>
+          </div>
+        </div>
       </div>
 
       <div className="Background"></div>
@@ -19,3 +29,5 @@ const Landing = () => {
   );
 };
 export default Landing;
+
+
